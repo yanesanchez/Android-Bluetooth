33e9d745c2eaa06fd301ecfe40355c17a88eb07f
yanesanchez <yaneli.dev@gmail.com>
2022-04-25 02:18:43 -0400

    Added sound button, added sound fragment to view

--- A12-BT/app/src/main/java/com/example/a12_bt/SoundFragment.java
+++ A12-BT/app/src/main/java/com/example/a12_bt/SoundFragment.java
@@ -8,25 +8,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
+import com.example.a12_bt.databinding.FragmentSoundBinding;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link SoundFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class SoundFragment extends Fragment {
+    FragmentSoundBinding binding;
 
     public SoundFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SoundFragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static SoundFragment newInstance(String param1, String param2) {
         SoundFragment fragment = new SoundFragment();
@@ -42,6 +38,9 @@ public class SoundFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        binding = FragmentSoundBinding.inflate(inflater, container, false);
+
+        
         return inflater.inflate(R.layout.fragment_sound, container, false);
     }
 }
