a845f895e66c4bb175c31cf1aea50d4749de7145
yanesanchez <yaneli.dev@gmail.com>
2022-04-24 23:48:14 -0400

    Implemented move backwards

--- A12-BT/app/src/main/java/com/example/a12_bt/DriveFragment.java
+++ A12-BT/app/src/main/java/com/example/a12_bt/DriveFragment.java
@@ -52,7 +52,6 @@ public class DriveFragment extends Fragment implements SeekBar.OnSeekBarChangeLi
         binding.vvSeekBar1.setOnSeekBarChangeListener(this);
         binding.vvSeekBar2.setOnSeekBarChangeListener(this);
 
-        // TODO DIRECTION BUTTONS
         binding.vvBtnFORWARD.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -152,56 +151,63 @@ public class DriveFragment extends Fragment implements SeekBar.OnSeekBarChangeLi
         }
         catch (Exception e) {
             // TODO add error to new textView
-            // binding.vvTvOut1.setText("Error in MoveForward(" + e.getMessage() + ")");
+            binding.vvTvStatusLabel.setText("Error in Move(" + e.getMessage() + ")");
         }
     }
 
     /* ^ FORWARD */
+    // Communication Developer Kit Page 27
+    // 4.2.2 Start motor B & C forward at power 50 for 3 rotation and braking at destination
     public void cpf_EV3MoveMotorFORWARD() {
         try {
             binding.vvTvStatusLabel.setText(R.string.moveForward);
-            /*
-            byte[] buffer = new byte[20];       // 0x12 command length
+
+            byte[] buffer = new byte[12];       // command length
 
             buffer[0] = (byte) (20-2);
             buffer[1] = 0;
 
-            buffer[2] = 34;
+            buffer[2] = 34;     // message counter
             buffer[3] = 12;
 
-            buffer[4] = (byte) 0x80;
+            buffer[4] = (byte) 0x80; // command type; direct command no reply
 
-            buffer[5] = 0;
+            buffer[5] = 0;      // header alloc
             buffer[6] = 0;
 
-            buffer[7] = (byte) 0xae;
-            buffer[8] = 0;
+            // Firmware Developer Kit, pg 56
+            // opOutput_Step_Speed (LAYER, NOS, SPEED, STEP1, STEP2, STEP3, BRAKE)
+            buffer[7] = (byte) 0xAE;    // --- Op Code
+            buffer[8] = 0;              // LAYER 0
 
+            // LCO(MOTOR_A + MOTOR_B)
             buffer[9] = (byte) 0x06;
 
-            buffer[10] = (byte) 0x81;
-            buffer[11] = (byte) 0x32;
+            // LC1(SPEED_50) - speed 50%
+            buffer[10] = (byte) 0x81;   // opUI_Read (CMD, ...) ; Firmware pg 90
+            buffer[11] = (byte) 0x32; // opMove8_32 (SOURCE, DESTINATION) ; pg 31
 
+            // LCO(0) - no step 1
             buffer[12] = 0;
 
+            // Communication Developer Kit, pg 27
+            // LC2(900) - STEP2 for 2.5 rotation, 900 degrees
             buffer[13] = (byte) 0x82;
             buffer[14] = (byte) 0x84;
             buffer[15] = (byte) 0x03;
 
-            buffer[16] = (byte) 0x82;
-            buffer[17] = (byte) 0xB4;
+            // LC2(180) - STEP3 for 0.5 rotation, 180 degrees
+            buffer[16] = (byte) 0x82;   // opUI_WRITE (CMD, ...) ; pg 94
+            buffer[17] = (byte) 0xB4;   // -> 180
             buffer[18] = (byte) 0x00;
 
-            buffer[19] = 1;
+            buffer[19] = 1; // BRAKE
 
             cv_os.write(buffer);
             cv_os.flush();
-
-             */
         }
         catch (Exception e) {
-            // TODO add error to new textView
-            // binding.vvTvOut1.setText("Error in MoveForward(" + e.getMessage() + ")");
+            binding.vvTvStatusLabel.setText("Error in MoveForward(" + e.getMessage() + ")");
         }
     }
 
@@ -209,48 +215,34 @@ public class DriveFragment extends Fragment implements SeekBar.OnSeekBarChangeLi
     public void cpf_EV3MoveMotorBACKWARD() {
         try {
             binding.vvTvStatusLabel.setText(R.string.moveBackwards);
-            /*
-            byte[] buffer = new byte[20];       // 0x12 command length
 
-            buffer[0] = (byte) (20-2);
+            byte[] buffer = new byte[12];       // command length
+
+            buffer[0] = (byte) (12-2);
             buffer[1] = 0;
 
-            buffer[2] = 34;
+            buffer[2] = 34;     // message counter
             buffer[3] = 12;
 
-            buffer[4] = (byte) 0x80;
+            buffer[4] = (byte) 0x80; // command type; direct command no reply
 
-            buffer[5] = 0;
+            buffer[5] = 0;      // header alloc
             buffer[6] = 0;
 
-            buffer[7] = (byte) 0xae;
+            // Firmware Developer Kit, pg 54
+            // opOutput_Polarity (LAYER, NOS, POL)
+            buffer[7] = (byte) 0xA7;    // --- Op Code
             buffer[8] = 0;
-
-            buffer[9] = (byte) 0x06;
-
-            buffer[10] = (byte) 0x81;
-            buffer[11] = (byte) 0x32;
-
-            buffer[12] = 0;
-
-            buffer[13] = (byte) 0x82;
-            buffer[14] = (byte) 0x84;
-            buffer[15] = (byte) 0x03;
-
-            buffer[16] = (byte) 0x82;
-            buffer[17] = (byte) 0xB4;
-            buffer[18] = (byte) 0x00;
-
-            buffer[19] = 1;
+            // arguments
+            buffer[9] = (byte) 0x06;    // LAYER
+            buffer[10] = (byte) 0x81;   // NOS - Output bit field
+            buffer[11] = (byte) 0xFF;   // POL - polarity[-1|0|1]; FF -> -1
 
             cv_os.write(buffer);
             cv_os.flush();
-
-             */
         }
         catch (Exception e) {
-            // TODO add error to new textView
-            // binding.vvTvOut1.setText("Error in MoveForward(" + e.getMessage() + ")");
+            binding.vvTvStatusLabel.setText("Error in MoveBackward(" + e.getMessage() + ")");
         }
     }
 
@@ -294,12 +286,10 @@ public class DriveFragment extends Fragment implements SeekBar.OnSeekBarChangeLi
 
             cv_os.write(buffer);
             cv_os.flush();
-
              */
         }
         catch (Exception e) {
-            // TODO add error to new textView
-            // binding.vvTvOut1.setText("Error in MoveForward(" + e.getMessage() + ")");
+            binding.vvTvStatusLabel.setText("Error in MoveLeft(" + e.getMessage() + ")");
         }
     }
 
@@ -343,12 +333,11 @@ public class DriveFragment extends Fragment implements SeekBar.OnSeekBarChangeLi
 
             cv_os.write(buffer);
             cv_os.flush();
-
              */
         }
         catch (Exception e) {
             // TODO add error to new textView
-            // binding.vvTvOut1.setText("Error in MoveForward(" + e.getMessage() + ")");
+            binding.vvTvStatusLabel.setText("Error in MoveRight(" + e.getMessage() + ")");
         }
     }
 
